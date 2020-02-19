4995ab27256158d412a4b58d90a83992874c136a
vindarel <vindarel@mailz.org>
2020-02-19 17:13:38 +0100

    price_fmt: defensive

--- bookshops/utils/scraperUtils.py
+++ bookshops/utils/scraperUtils.py
@@ -182,6 +182,8 @@ def price_fmt(price, currency):
 
     Exemple: 10 € or CHF 10
     """
+    if price is None or isinstance(price, str) or not currency:
+        return price
     if currency.lower() == 'chf':
         return 'CHF {:.2f}'.format(price)
     else:
